c31e2bc76f96e842e3a107e7f3edb6ba3091a7c9
Lee YoungCheol <ofetwo@gmail.com>
2020-06-10 02:40:20 +0900

    Update lecture02

---
+++ lecture02/printf4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void say_hello(void)
+{
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+}
+
+int main()
+{
+    say_hello();
+
+    return 0;
+}
---
+++ lecture02/printf5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void say_hello(void); // prototype, function declaration
+
+int main()
+{
+    say_hello();
+
+    return 0;
+}
+
+void say_hello(void) // function definition
+{
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+}
