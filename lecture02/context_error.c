202ece8a8b29c4c089d66ee1a7f16b07874dca3c
Lee YoungCheol <ofetwo@gmail.com>
2020-06-10 04:34:50 +0900

    Update lecture02

--- lecture02/context_error.c
+++ lecture02/context_error.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
+// syntax errors can be easily detected
 int main()
 {
     int n1, n2, n3, n4;
-
-    n1 = 1;
+    //hardly can know purpose of program...
+    n1 = 1;      
     n2 = n1 * n1;
     n3 = n2 * n1;
     n4 = n2 * n1;
