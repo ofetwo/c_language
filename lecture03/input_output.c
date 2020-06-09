7400d37d084afed616db547f7bfc528daa2cf3a9
Lee YoungCheol <ofetwo@gmail.com>
2020-06-10 05:56:37 +0900

    Upload lecture03

---
+++ lecture03/input_output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0, j = 0;
+
+    scanf("%d%d", &i, &j);
+
+    printf("Value is %d\n", i);
+
+    return 0;
+}
---
+++ lecture03/scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+
+    scanf("%d", &i); // & : ampersand       
+
+    printf("Value is %d\n", i);
+
+    return 0;
+}
